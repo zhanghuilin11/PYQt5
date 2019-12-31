c9d0572001c1edc5bada1035eff177a945d03390
zhanghuilin11 <azhanghuilin@163.com>
2019-12-31 18:09:02 +0800

    show test

---
+++ GUIlearn/runMain.py
@@ -0,0 +1,11 @@
+import sys
+import main
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = main.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
