2d0ec3ad98b1c97329054e468cf8c525ee116089
zhanghuilin11 <azhanghuilin@163.com>
2020-01-03 21:01:42 +0800

    pypyqt5

---
+++ PYQt5/src/designer/py-pyqt5-chutiyan.py
@@ -0,0 +1,16 @@
+from PyQt5.Qt import *
+import sys
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle('就不给你充电宝')
+    window.resize(120,100)
+    #window.move(400,400)
+    label = QLabel(window)
+    label.setText('fgjhdfgjkdfhhdfjhdfhjdfhjdhd')
+    label.move(50,50)
+    # label.show()
+    window.show()
+    sys.exit(app.exec_())
+    # while True:
+    #     pass
